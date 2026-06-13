3bb9dab29d65f8998c369a77429a60582253a137
Karen Kaminsky <karenkaminskysilvaa@gmail.com>
2026-06-13 18:23:07 -0300

    Starting to implement Board and Piece

---
+++ chess_system/src/boardgame/Board.java
@@ -0,0 +1,13 @@
+package boardgame;
+
+public class Board {
+	private int row;
+	private int columns;
+	private Piece[][] pieces;
+
+	public Board(int row, int columns) {
+		this.row = row;
+		this.columns = columns;
+	}
+
+}
