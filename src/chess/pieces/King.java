29e850c95567c6952a70f58c6a8e5dfb9425d625
Karen Kaminsky <karenkaminskysilvaa@gmail.com>
2026-07-11 20:04:23 -0300

    Moving pieces

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -6,6 +6,7 @@ import chess.Color;
 
 public class King extends ChessPiece {
 
+//Esse construtor inicializa o Rei chamando o construtor da classe base com o tabuleiro e a cor
 	public King(Board board, Color color) {
 		super(board, color);
 	}
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -6,6 +6,7 @@ import chess.Color;
 
 public class Rook extends ChessPiece{
 	
+//Esse construtor inicializa a Torre chamando o construtor da classe base com o tabuleiro e a cor
 	public Rook(Board board, Color color) {
 		super(board, color);
 	}
