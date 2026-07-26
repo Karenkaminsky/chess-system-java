22ff66ccad46f1e72ca2d559c5fcfa4b5577a2b6
Karen Kaminsky <karenkaminskysilvaa@gmail.com>
2026-07-26 14:41:03 -0300

    Special move - Castling

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,15 +1,22 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
 
-//Esse construtor inicializa o Rei chamando o construtor da classe base com o tabuleiro e a cor
-	public King(Board board, Color color) {
+	// Referência para a partida de xadrez
+	private ChessMatch chessMatch;
+
+	// Esse construtor inicializa o Rei chamando o construtor da classe base com o
+	// tabuleiro e a cor
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 
 	@Override
@@ -22,8 +29,16 @@ public class King extends ChessPiece {
 		return p == null || p.getColor() != getColor();
 	}
 
+	// Verifica se a torre pode realizar o roque
+	private boolean testRookCastling(Position position) {
+		Piece p = getBoard().piece(position);
+		return p != null && p instanceof Rook && ((ChessPiece) p).getColor() == getColor()
+				&& ((ChessPiece) p).getMoveCount() == 0;
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
+
 		// Matriz temporária
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
@@ -75,6 +90,32 @@ public class King extends ChessPiece {
 		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// #Special Move - Castling Kingside Rook (Movimento especial: Roque pequeno)
+		if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+			// #specialmove castling kingside rook
+			Position posT1 = new Position(position.getRow(), position.getColumn() + 3);
+
+			if (testRookCastling(posT1)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+					mat[position.getRow()][position.getColumn() + 2] = true;
+				}
+			}
+
+			// #Special Move - Castling Queenside Rook (Movimento especial: Roque grande)
+			Position posT2 = new Position(position.getRow(), position.getColumn() - 4);
+			if (testRookCastling(posT2)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+				Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+					mat[position.getRow()][position.getColumn() - 2] = true;
+				}
+			}
 		}
 
 		return mat;
